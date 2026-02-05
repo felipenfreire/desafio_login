27cc407cd6c946963cfc20b63c0de629fe5df12a
Felipe do Nascimento Freire <felipenascimento871@gmail.com>
2026-02-05 18:27:21 -0300

    Segundo commit de hj

---
+++ exercicio.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite aqui sua idade: '))
+
+print(f'Olá, {nome}!!')
+print(f'{nome}, seu nome tem {len(nome)} letras.')
+print(f'Daqui a 5 anos vc terá {idade + 5} anos de idade!')
